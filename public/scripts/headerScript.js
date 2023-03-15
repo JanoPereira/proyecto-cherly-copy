66625257f847d30d3d16bfdcd7a67fb3b0517a31
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-03-15 16:45:50 -0400

    fixing burger menu toggle

--- public/scripts/headerScript.js
+++ public/scripts/headerScript.js
@@ -13,38 +13,48 @@ window.addEventListener('load', () => {
         header.style.backgroundColor = 'white'
     }
 
-    
+
 
     burgerIcon.classList.add('icon-active')
     xIcon.classList.add('icon-inactive')
-    const togglerButtonContainer = document.querySelector('.burguer-menu-toggler-container');
+    const togglerButton = document.querySelectorAll('.burguer-menu-toggler');
+
     const listenTogglerButton = () => {
-        let togglerButton = document.querySelector('.burguer-menu-toggler');
-        
-        togglerButton.addEventListener('click', () => {
-            if (togglerButton.classList.contains('close-button')) { // CIERRA MENU
-                sideNavbar.classList.remove('side-navbar-active')
-                burgerIcon.classList.add('icon-active')
-                burgerIcon.classList.remove('icon-inactive')
-                xIcon.classList.add('icon-inactive')
-                xIcon.classList.remove('icon-active')
-                if (isNotInHome) {
+
+        if (isNotInHome) {
+            header.style.backgroundColor = 'white'
+        }
+
+        togglerButton.forEach(btn => {
+            btn.addEventListener('click', () => {
+                if (btn.classList.contains('fa-x')) {
+                    console.log('entro al primero')
+                    sideNavbar.classList.remove('side-navbar-active')
+                    burgerIcon.classList.add('icon-active')
+                    burgerIcon.classList.remove('icon-inactive')
+                    xIcon.classList.add('icon-inactive')
+                    xIcon.classList.remove('icon-active')
+                    if (isNotInHome) {
+                        header.style.backgroundColor = 'white'
+                    }
+
+
+                } else {
+                    console.log('entro al segundo')
+                    sideNavbar.classList.add('side-navbar-active')
+                    burgerIcon.classList.add('icon-inactive')
+                    burgerIcon.classList.remove('icon-active')
+                    xIcon.classList.add('icon-active')
+                    xIcon.classList.remove('icon-inactive')
                     header.style.backgroundColor = 'white'
+
                 }
+            })
+        })
+
+    
 
 
-            } else { // ABRE MENU
-                sideNavbar.classList.add('side-navbar-active')
-                burgerIcon.classList.add('icon-inactive')
-                burgerIcon.classList.remove('icon-active')
-                xIcon.classList.add('icon-active')
-                xIcon.classList.remove('icon-inactive')
-                header.style.backgroundColor = 'white'
-                
-            }
-            listenTogglerButton();
-
-        });
     }
 
     listenTogglerButton();

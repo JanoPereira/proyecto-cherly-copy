f5d68ef38d55f0ab96c23029d346d2bb07d8b6de
Jano <janoo.pereira@gmail.com>
2023-03-14 23:43:33 -0300

    Arrancada la pagina de lista de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,7 +6,7 @@ const controller = {
             let products = await db.Product.findAll({
                 include:['category']
             });
-            return res.send(products)
+            return res.render('productList')
         } catch (error) {
             console.log(`Falle en productController.list: ${error}`);
             return res.json(error);

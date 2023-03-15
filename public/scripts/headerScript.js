8082a5e23bf96d98675a84819b99c4dfd5b0cb65
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-03-15 10:42:27 -0400

    working in burger menu and sidebar

--- public/scripts/headerScript.js
+++ public/scripts/headerScript.js
@@ -1,22 +1,52 @@
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
     // FUNCIONES
-    const listenTogglerButton = () =>{
+
+    const header = document.querySelector('.header-section');
+    let burgerIcon = document.querySelector('.fa-bars')
+    let xIcon = document.querySelector('.fa-x')
+    const sideNavbar = document.querySelector('.side-navbar')
+
+    const isNotInHome = window.location.pathname != '/'
+
+    if (isNotInHome) { // agregar estilo de header si no coincide con home
+        header.style.position = 'static'
+        header.style.backgroundColor = 'white'
+    }
+
+    
+
+    burgerIcon.classList.add('icon-active')
+    xIcon.classList.add('icon-inactive')
+    const togglerButtonContainer = document.querySelector('.burguer-menu-toggler-container');
+    const listenTogglerButton = () => {
         let togglerButton = document.querySelector('.burguer-menu-toggler');
-        let burguerMenu = document.querySelector('.burguer-menu-container')
-        togglerButton.addEventListener('click',()=>{
-            if (togglerButton.classList.contains('close-button')){ //Quiere decir que toco la x
-                togglerButtonContainer.innerHTML = `<i class="fa-solid fa-bars burguer-menu-toggler" ></i>`
-                burguerMenu.classList.add('hidden');
-            } else { //Toco el menu
-                togglerButtonContainer.innerHTML = `<i class="fa-solid fa-x burguer-menu-toggler close-button"></i>`
-                burguerMenu.classList.remove('hidden');
+        
+        togglerButton.addEventListener('click', () => {
+            if (togglerButton.classList.contains('close-button')) { // CIERRA MENU
+                sideNavbar.classList.remove('side-navbar-active')
+                burgerIcon.classList.add('icon-active')
+                burgerIcon.classList.remove('icon-inactive')
+                xIcon.classList.add('icon-inactive')
+                xIcon.classList.remove('icon-active')
+                if (isNotInHome) {
+                    header.style.backgroundColor = 'white'
+                }
+
+
+            } else { // ABRE MENU
+                sideNavbar.classList.add('side-navbar-active')
+                burgerIcon.classList.add('icon-inactive')
+                burgerIcon.classList.remove('icon-active')
+                xIcon.classList.add('icon-active')
+                xIcon.classList.remove('icon-inactive')
+                header.style.backgroundColor = 'white'
+                
             }
             listenTogglerButton();
-            
+
         });
-        
     }
-    const togglerButtonContainer = document.querySelector('.burguer-menu-toggler-container');
+
     listenTogglerButton();
-    
+
 })
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,9 +3,9 @@ const db = require('../database/models');
 const controller = {
     list: async(req,res)=>{
         try {
-            let products = await db.Product.findAll({
+            /* let products = await db.Product.findAll({
                 include:['category']
-            });
+            }); */
             return res.render('productList')
         } catch (error) {
             console.log(`Falle en productController.list: ${error}`);

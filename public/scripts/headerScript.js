ee2d37c840a5cab67007adff159228df11cf9730
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-03-17 15:50:47 -0400

    working on submenu

--- public/scripts/headerScript.js
+++ public/scripts/headerScript.js
@@ -5,14 +5,17 @@ window.addEventListener('load', () => {
     let burgerIcon = document.querySelector('.fa-bars')
     let xIcon = document.querySelector('.fa-x')
     const sideNavbar = document.querySelector('.side-navbar')
-
+    const sideSubmenuNavbar = document.querySelector('.side-navbar-submenu')
+    const giftId = document.getElementById('gifts')
+    const burgerSubmenuBack = document.querySelector('.burger-submenu-back')
     const isNotInHome = window.location.pathname != '/'
 
     if (isNotInHome) { // agregar estilo de header si no coincide con home
         header.style.position = 'static'
         header.classList.add('header-section-active')
     }
-
+    
+    console.log(sideNavbar.dataset.id)
 
 
     burgerIcon.classList.add('icon-active')
@@ -28,17 +31,18 @@ window.addEventListener('load', () => {
         togglerButton.forEach(btn => {
             btn.addEventListener('click', () => {
                 if (btn.classList.contains('fa-x')) { // CERRAR MENU
-                   
+
                     sideNavbar.classList.remove('side-navbar-active')
                     burgerIcon.classList.add('icon-active')
                     burgerIcon.classList.remove('icon-inactive')
                     xIcon.classList.add('icon-inactive')
                     xIcon.classList.remove('icon-active')
+                    sideSubmenuNavbar.classList.remove('side-navbar-submenu-active')
                     if (!isNotInHome) {
                         header.classList.remove('header-section-active')
                         header.style.backgroundColor = 'transparent'
                         header.style.position = 'absolute'
-                    } 
+                    }
 
 
                 } else {    // ABRIR MENU
@@ -49,17 +53,52 @@ window.addEventListener('load', () => {
                     xIcon.classList.add('icon-active')
                     xIcon.classList.remove('icon-inactive')
                     header.style.backgroundColor = 'white'
-                   
+
 
                 }
             })
         })
 
-    
+
 
 
     }
 
     listenTogglerButton();
+
+    const navbarMenusInteraction = () => {
+        giftId.addEventListener('click', () => {
+            sideNavbar.classList.remove('side-navbar-active')
+            sideSubmenuNavbar.classList.toggle('side-navbar-submenu-active')
+        })
+
+        burgerSubmenuBack.addEventListener('click', () => {
+            sideNavbar.classList.add('side-navbar-active')
+            sideSubmenuNavbar.classList.remove('side-navbar-submenu-active')
+        })
+    }
+
+    navbarMenusInteraction()
+
+    const headerShow = () => {
+        let prevScrollPos = window.pageYOffset;
+        window.onscroll = function () {
+            let currentScrollPos = window.pageYOffset;
+            if (prevScrollPos > currentScrollPos) {
+                console.log("El usuario está haciendo scroll hacia arriba");
+                header.classList.add('header-section-active')
+            } else {
+                header.classList.remove('header-section-active')
+                console.log("El usuario está haciendo scroll hacia abajo");
+            }
+            prevScrollPos = currentScrollPos;
+        }
+    }
+
+    headerShow()
+
+
+
+
 
 })

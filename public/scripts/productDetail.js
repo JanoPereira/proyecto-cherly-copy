31fe4e3b77240716eb6509e32e377e92e893cf18
Jano <janoo.pereira@gmail.com>
2023-03-28 19:17:49 -0300

    Modificada la seccion del dettalle del producto para desktop

--- public/scripts/productDetail.js
+++ public/scripts/productDetail.js
@@ -4,13 +4,31 @@ window.addEventListener('load', () => {
     var images = document.querySelectorAll('.product-image');
     var imageWidth = images[0].offsetWidth;
     var imageShowed = 0
-    
-    carouselImages.addEventListener('scroll', function (e) {
-        
-        
-        
-    });
-  
 
+    carouselImages.addEventListener('scroll', function (e) {
+
+
+
+    });
+
+    // LOGICA PARA TOCAR LA FOTO Y CAMBIARLA
+    const carouselImagesContainer = document.querySelector('.carousel-images-container');
+    const sideImages = document.querySelectorAll('.side-image');
+    sideImages.forEach(img => {
+        img.addEventListener('click', () => {
+
+            // Para sacarle la clase active a la foto anterior
+            document.querySelector('.product-image-active').classList.remove('product-image-active');
+            
+            // Tengo que crear una etiqueta para agregarsela al div
+            const newImg = document.createElement('img');
+            newImg.src = img.getAttribute('src');
+            newImg.classList.add('product-image');
+            img.classList.add('product-image-active');
+            carouselImagesContainer.innerHTML = '';
+            carouselImagesContainer.appendChild(newImg);
+
+        })
+    })
 
 });

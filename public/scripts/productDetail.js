94f4ce696e8aaad8f52aef5fd48fdf36e7230f08
Jano <janoo.pereira@gmail.com>
2023-03-28 22:06:32 -0300

    Casi que terminada la nueva vista de productDetail

--- public/scripts/productDetail.js
+++ public/scripts/productDetail.js
@@ -1,3 +1,4 @@
+import { activateClass, deactivateClass } from "./utils.js";
 window.addEventListener('load', () => {
     var carouselImages = document.querySelector('.carousel-images-container');
     var carouselBar = document.querySelector('.scrollbar-carousel');
@@ -19,7 +20,7 @@ window.addEventListener('load', () => {
 
             // Para sacarle la clase active a la foto anterior
             document.querySelector('.product-image-active').classList.remove('product-image-active');
-            
+
             // Tengo que crear una etiqueta para agregarsela al div
             const newImg = document.createElement('img');
             newImg.src = img.getAttribute('src');
@@ -29,6 +30,36 @@ window.addEventListener('load', () => {
             carouselImagesContainer.appendChild(newImg);
 
         })
+    });
+
+    // LOGICA PARA MOSTRAR SIZE GUIDE & PRODUCT SPECIFICATIONS
+    const sizeGuideBtn = document.getElementById('size-guide-btn');
+    const productSpecificationsBtn = document.getElementById('product-specifications-btn');
+    const closeSizeGuideBtns = document.querySelectorAll('.close-size-guide-btn');
+    const body = document.querySelector('body');
+
+    const blackScreen = 'black-screen';
+    const sizeGuideImgContainer = 'size-guide-img-container';
+    const productSpecificationsContainer = 'product-specifications-container';
+
+    sizeGuideBtn.addEventListener('click', () => {
+        body.classList.add('noScroll');
+        activateClass([blackScreen, sizeGuideImgContainer]);
+    });
+    productSpecificationsBtn.addEventListener('click', () => {
+        body.classList.add('noScroll');
+        activateClass([blackScreen, productSpecificationsContainer]);
     })
+    document.querySelector(`.${blackScreen}`).addEventListener('click', () => {
+        body.classList.remove('noScroll');
+        deactivateClass([blackScreen, sizeGuideImgContainer, productSpecificationsContainer]);
+    });
+    closeSizeGuideBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            body.classList.remove('noScroll');
+            deactivateClass([blackScreen, sizeGuideImgContainer, productSpecificationsContainer]);
+        })
+    });
+
 
 });
